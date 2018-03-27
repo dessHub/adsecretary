de270d1a4058b132b661598607c0e2258ce348ec
desshub <desshub95@gmail.com>
2018-03-27 22:45:38 +0300

    initial commit

---
+++ resources/views/home.blade.php
@@ -0,0 +1,83 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container-fluid">
+ <div class="slider">
+    <div class="row">
+        <div class="col-md-12 col-xs-12">
+          <h3>THIS IS AD SECRETARY</h3>
+
+          <div class="col-md-offset-3 col-md-5 col-xs-0ffset-1 col-xs-10">
+             <span><input class="form-control" type="text"></span>
+             <span>Search</span>
+           </div>
+        </div>
+    </div>
+  </div>
+</div>
+
+<div class="container">
+ <div class="top-ads">
+   <div class="section-title text-center">
+     <h2>Top Ad Spaces</h2>
+      <hr>
+   </div>
+   <div class="row">
+       <div class="col-md-3 col-xs-12">
+      <div class="ad-top text-center">
+        <img src="/images/default.gif">
+        <div class="ad-info">
+          <p>category</p>
+          <p>Running Time</p>
+          <p>Duration</p>
+          <p>Price</p>
+        
+           <div class="text-center"><button class="btn btn-info">More Info</button></div>
+        </div>
+      </div>
+    </div>   <div class="col-md-3 col-xs-12">
+      <div class="ad-top text-center">
+        <img src="/images/default.gif">
+        <div class="ad-info">
+          <p>category</p>
+          <p>Running Time</p>
+          <p>Duration</p>
+          <p>Price</p>
+        
+           <div class="text-center"><button class="btn btn-info">More Info</button></div>
+        </div>
+      </div>
+    </div>   <div class="col-md-3 col-xs-12">
+      <div class="ad-top text-center">
+        <img src="/images/default.gif">
+        <div class="ad-info">
+          <p>category</p>
+          <p>Running Time</p>
+          <p>Duration</p>
+          <p>Price</p>
+        
+           <div class="text-center"><button class="btn btn-info">More Info</button></div>
+        </div>
+      </div>
+    </div>   <div class="col-md-3 col-xs-12">
+      <div class="ad-top text-center">
+        <img src="/images/default.gif">
+        <div class="ad-info">
+          <p>category</p>
+          <p>Running Time</p>
+          <p>Duration</p>
+          <p>Price</p>
+        
+           <div class="text-center"><button class="btn btn-info">More Info</button></div>
+        </div>
+      </div>
+    </div>
+
+   </div>
+ </div>
+
+ <div class="section-bottom">
+  <h3>Browse More</h3>
+ </div>
+</div>
+@endsection
