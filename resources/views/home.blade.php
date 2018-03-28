1ab6bc3f1355011b9c2a2897ca20808ff70f4d97
desshub <desshub95@gmail.com>
2018-03-28 13:53:12 +0300

    designed landing page

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,17 +3,25 @@
 @section('content')
 <div class="container-fluid">
  <div class="slider">
+  <div class="slider-cover">
     <div class="row">
         <div class="col-md-12 col-xs-12">
-          <h3>THIS IS AD SECRETARY</h3>
-
-          <div class="col-md-offset-3 col-md-5 col-xs-0ffset-1 col-xs-10">
+         <div class="slider-content">
+           <div class="col-md-offset-3 col-md-6 col-xs-12">
+            <h3>THIS IS AD SECRETARY</h3>
+            <hr class="slider-hr">
+            <p>Are you urgently looking for a ad space! Well Look no more, ADSecretary got your back. Get connected with the best media house with the best price. </p>
+            <div class="col-md-offset-3 col-md-5 col-xs-0ffset-1 col-xs-10">
              <span><input class="form-control" type="text"></span>
              <span>Search</span>
+
+            </div>
            </div>
+         </div>
         </div>
     </div>
   </div>
+ </div>
 </div>
 
 <div class="container">
